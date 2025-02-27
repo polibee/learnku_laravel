3c143e671919492d72b2bd1c3d44417cbd52c297
polibee <1244762327@qq.com>
2025-02-27 18:23:59 +0800

    粉丝数据表

--- app/Policies/StatusPolicy.php
+++ app/Policies/StatusPolicy.php
@@ -9,15 +9,9 @@ use App\Models\Status;
 class StatusPolicy
 {
     use HandlesAuthorization;
-    /**
-     * Create a new policy instance.
-     */
-    public function __construct()
+
+    public function destroy(User $user, Status $status)
     {
-        //
-    }
-    public function destroy(User $user,Status $status)
-    {
-        return $user->id === $status->user_id;
+        return $user->is_admin || $user->id === $status->user_id;
     }
 }
--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -9,6 +9,11 @@ class UserPolicy
 {
     use HandlesAuthorization;
 
+    public function update(User $currentUser, User $user)
+    {
+        return $currentUser->id === $user->id;
+    }
+
     public function destroy(User $currentUser, User $user)
     {
         return $currentUser->is_admin && $currentUser->id !== $user->id;
